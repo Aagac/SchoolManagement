ec6bc99d68e2834ce94354e511877fd29d652d57
Yildiz <yldzgny34@gmail.com>
2023-04-14 10:14:13 +0200

    entitý classes added ikinci version

---
+++ src/yildiz/Ogretmen.java
@@ -0,0 +1,48 @@
+package yildiz;
+
+public class Ogretmen extends Kisi {
+    private String bolum;
+    private String sicilNo;
+
+    public Ogretmen() {
+    }
+
+    public Ogretmen(String name, String lastName, String kimlikNo, int yas) {
+        super(name, lastName, kimlikNo, yas);
+    }
+
+    public Ogretmen(String bolum, String sicilNo) {
+        this.bolum = bolum;
+        this.sicilNo = sicilNo;
+    }
+
+    public Ogretmen(String name, String lastName, String kimlikNo, int yas, String bolum, String sicilNo) {
+        super(name, lastName, kimlikNo, yas);
+        this.bolum = bolum;
+        this.sicilNo = sicilNo;
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public String getSicilNo() {
+        return sicilNo;
+    }
+
+    public void setSicilNo(String sicilNo) {
+        this.sicilNo = sicilNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Ogretmen{" +
+                "bolum='" + bolum + '\'' +
+                ", sicilNo='" + sicilNo + '\'' +
+                '}';
+    }
+}
