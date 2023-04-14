8fb71af35bef8f7a7eefe257579c363d7d480d07
Ahmet Agac <a.agac0668@gmail.com>
2023-04-14 12:57:43 +0200

    eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        boolean flag =true;
-
-        flag =false;
-
-        if ( flag ) System.out.println("404 not found ogrenci bulunamadi");
-
     }
 }
