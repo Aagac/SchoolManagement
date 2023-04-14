c16a85a586469eb29fec08c3e481b364f6880b4f
Ahmet Agac <a.agac0668@gmail.com>
2023-04-14 12:57:43 +0200

    eklendi

--- src/ahmet/Islemler.java
+++ src/ahmet/Islemler.java
@@ -1,20 +1,21 @@
 package ahmet;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import static ahmet.Runner.run;
 
-public class Islemler extends Kisi {
+public class Islemler {
 
     static String kisiTuru;
-    static Scanner scn = new Scanner(System.in);
-    static List<Kisi> ogrenciList = new ArrayList<>();
-    static List<Kisi> ogretmenList = new ArrayList<>();
-    Ogrenci ogr = new Ogrenci();
+    static Scanner scn=new Scanner(System.in);
+    static List<Kisi> ogrenciList=new ArrayList<>();
+    static List<Kisi> ogretmenList=new ArrayList<>();
 
 
-    public void islemMenusu() {
-        System.out.println("sectigin kisi turu " + kisiTuru + " ***" +
+    public void islemMenusu(){
+        System.out.println("sectigin kisi turu "+kisiTuru+" ***"+
                 " asagidaki islemlerden hangisini yapmak istiyorsun");
 
         System.out.println("============= İŞLEMLER =============\n" +
@@ -23,22 +24,22 @@ public class Islemler extends Kisi {
                 "\t\t 3-LİSTELEME\n" +
                 "\t\t 4-SİLME\n" +
                 "\t\t 5-ANA MENÜ\n");
-        int secilenIslem = scn.nextInt();
-        switch (secilenIslem) {
+        int secilenIslem= scn.nextInt();
+        switch (secilenIslem){
             case 1:
                 ekleme();
                 islemMenusu();
                 break;
             case 2:
-                arama();
+               // arama();
                 islemMenusu();
                 break;
             case 3:
-                //listeleme();
+               // listeleme();
                 islemMenusu();
                 break;
             case 4:
-                // silme();
+               // silme();
                 islemMenusu();
                 break;
             case 5:
@@ -52,80 +53,53 @@ public class Islemler extends Kisi {
         }
 
 
-    }
-
-    private void arama() {
-        System.err.println("*** " + kisiTuru + " *** ekleme sayfasina hosgeldiniz");
-        boolean flag=true;//bulamama ihtimaline karsin create edildi
-
-        if (kisiTuru.equalsIgnoreCase("OGRENCI")){
-            System.out.println("Kimlik no gir");
-            String kimlikNo= scn.next();
-
-            for (Kisi each:ogrenciList) {
-                if (each.getKimlikNo().equalsIgnoreCase(kimlikNo)){
-                    System.out.println("aradigin ögrenci "+each.getName());
-                    flag=false;// false oldugu icin loop durdu disardaki if calismayacak
-                }
-            }
-            if (flag) System.out.println("404 not found ogrenci bulunamadi");
-        }else {
-            System.out.println("Kimlik no gir");
-            String kimlikNo= scn.next();
-
-            for (Kisi each:ogretmenList) {
-                if (each.getKimlikNo().equalsIgnoreCase(kimlikNo)){
-                    System.out.println("aradigin ögrenci "+each.getName());
-                    flag=false;// false oldugu icin loop durdu disardaki if calismayacak
-                }
-            }
-            if (flag) System.out.println("404 not found ogrenci bulunamadi");
-        }
-
-
-
-
 
     }
 
     private void ekleme() {
-        System.err.println("*** " + kisiTuru + " *** ekleme sayfasina hosgeldiniz");
+        System.err.println("*** "+kisiTuru+" *** ekleme sayfasina hosgeldiniz");
         scn.nextLine();
         System.out.println("adini gir");
-        String adi = scn.nextLine();
+        String adi= scn.nextLine();
 
         System.out.println("soyadini gir");
-        String soyad = scn.next();
+        String soyad= scn.next();
 
         System.out.println("kimlik no gir");
-        String kimlikNo = scn.next();
+        String kimlikNo= scn.next();
 
         System.out.println("yasini gir");
-        int yas = scn.nextInt();
+        int yas= scn.nextInt();
 
-        if (kisiTuru.equalsIgnoreCase("OGRENCI")) {//ogrenci icin
+        if (kisiTuru.equalsIgnoreCase("OGRENCI")){//ogrenci icin
             System.out.println("ogrenci no gir");
-            int ogrenciNo = scn.nextInt();
+            int ogrenciNo= scn.nextInt();
 
             System.out.println("sinifini gir");
-            String sinif = scn.next();
+            String sinif= scn.next();
 
-            Ogrenci ogrenci = new Ogrenci(adi, soyad, kimlikNo, yas, ogrenciNo, sinif);
+            Ogrenci ogrenci=new Ogrenci(adi,soyad,kimlikNo,yas,ogrenciNo,sinif);
             System.out.println("ogrenciList.add(ogrenci) = " + ogrenciList.add(ogrenci));
             System.out.println("ogrenci = " + ogrenci);
 
-        } else {//ogretmen icin
+        }else {//ogretmen icin
             System.out.println("bolum gir");
-            String bolum = scn.next();
+            String bolum=scn.next();
 
             System.out.println("sicil no gir");
-            String sicilNo = scn.next();
+            String sicilNo= scn.next();
 
-            Ogretmen ogretmen = new Ogretmen(adi, soyad, kimlikNo, yas, bolum, sicilNo);
+            Ogretmen ogretmen=new Ogretmen(adi,soyad,kimlikNo,yas,bolum,sicilNo);
             System.out.println("ogretmenList.add(ogretmen) = " + ogretmenList.add(ogretmen));
             System.out.println("ogretmenList = " + ogretmenList);
 
         }
+
+
+
+
+
+
 
 
     }
