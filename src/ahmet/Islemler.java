54f2ad95b54fc690cf53db7a8f4e4b195082dfbb
Ahmet Agac <a.agac0668@gmail.com>
2023-04-14 12:57:43 +0200

    eklendi

---
+++ src/ahmet/Islemler.java
@@ -0,0 +1,108 @@
+package ahmet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static ahmet.Runner.run;
+
+public class Islemler {
+
+    static String kisiTuru;
+    static Scanner scn=new Scanner(System.in);
+    static List<Kisi> ogrenciList=new ArrayList<>();
+    static List<Kisi> ogretmenList=new ArrayList<>();
+
+
+    public void islemMenusu(){
+        System.out.println("sectigin kisi turu "+kisiTuru+" ***"+
+                " asagidaki islemlerden hangisini yapmak istiyorsun");
+
+        System.out.println("============= İŞLEMLER =============\n" +
+                "\t\t 1-EKLEME\n" +
+                "\t\t 2-ARAMA\n" +
+                "\t\t 3-LİSTELEME\n" +
+                "\t\t 4-SİLME\n" +
+                "\t\t 5-ANA MENÜ\n");
+        int secilenIslem= scn.nextInt();
+        switch (secilenIslem){
+            case 1:
+                ekleme();
+                islemMenusu();
+                break;
+            case 2:
+               // arama();
+                islemMenusu();
+                break;
+            case 3:
+               // listeleme();
+                islemMenusu();
+                break;
+            case 4:
+               // silme();
+                islemMenusu();
+                break;
+            case 5:
+                run.anaManu();
+                break;
+            default:
+                System.out.println("yanlis tuslama yaptin tekrar dene");
+                islemMenusu();
+                break;
+
+        }
+
+
+
+    }
+
+    private void ekleme() {
+        System.err.println("*** "+kisiTuru+" *** ekleme sayfasina hosgeldiniz");
+        scn.nextLine();
+        System.out.println("adini gir");
+        String adi= scn.nextLine();
+
+        System.out.println("soyadini gir");
+        String soyad= scn.next();
+
+        System.out.println("kimlik no gir");
+        String kimlikNo= scn.next();
+
+        System.out.println("yasini gir");
+        int yas= scn.nextInt();
+
+        if (kisiTuru.equalsIgnoreCase("OGRENCI")){//ogrenci icin
+            System.out.println("ogrenci no gir");
+            int ogrenciNo= scn.nextInt();
+
+            System.out.println("sinifini gir");
+            String sinif= scn.next();
+
+            Ogrenci ogrenci=new Ogrenci(adi,soyad,kimlikNo,yas,ogrenciNo,sinif);
+            System.out.println("ogrenciList.add(ogrenci) = " + ogrenciList.add(ogrenci));
+            System.out.println("ogrenci = " + ogrenci);
+
+        }else {//ogretmen icin
+            System.out.println("bolum gir");
+            String bolum=scn.next();
+
+            System.out.println("sicil no gir");
+            String sicilNo= scn.next();
+
+            Ogretmen ogretmen=new Ogretmen(adi,soyad,kimlikNo,yas,bolum,sicilNo);
+            System.out.println("ogretmenList.add(ogretmen) = " + ogretmenList.add(ogretmen));
+            System.out.println("ogretmenList = " + ogretmenList);
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/ahmet/Ogretmen.java
@@ -0,0 +1,46 @@
+package ahmet;
+
+public class Ogretmen extends Kisi {
+
+    private String bolum;
+    private String sicilNo;
+
+    public Ogretmen(String name, String lastName, String kimlikNo, int yas, String bolum, String sicilNo) {
+        super(name, lastName, kimlikNo, yas);
+        this.bolum = bolum;
+        this.sicilNo = sicilNo;
+    }
+
+    public Ogretmen(String bolum, String sicilNo) {
+        this.bolum = bolum;
+        this.sicilNo = sicilNo;
+    }
+
+    public Ogretmen() {
+
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public String getSicilNo() {
+        return sicilNo;
+    }
+
+    public void setSicilNo(String sicilNo) {
+        this.sicilNo = sicilNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Ogretmen{" +
+                "bolum='" + bolum + '\'' +
+                ", sicilNo='" + sicilNo + '\'' +super.toString()+
+                '}';
+    }
+}
