72b9ddcdb99fb62dbfdd90b1a12820a7599134e0
zaferCW <zafer@clarusway.com>
2023-04-18 13:29:38 +0300

    arama method added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        boolean flag =true;
+
+        flag =false;
+
+        if ( flag ) System.out.println("404 not found ogrenci bulunamadi");
+
     }
 }
