abb60cef4dac856d07d42698fa04addf5500214a
zaferCW <zafer@clarusway.com>
2023-04-19 13:38:20 +0300

    apdate method added

--- src/zafer/Ogretmen.java
+++ src/zafer/Ogretmen.java
@@ -38,9 +38,9 @@ public class Ogretmen extends Kisi{
     @Override
     public String toString() {
         return "Ogretmen{" +
+                super.toString()+
                 "bolum='" + bolum + '\'' +
                 ", sicilNo='" + sicilNo + '\'' +
-                super.toString()+
                 '}';
     }
 }
